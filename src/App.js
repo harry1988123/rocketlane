5407e03ab5d82cf421de1ea5050506acda468a2d
Harry <harendra.singh2015@vit.ac.in>
2021-10-30 03:48:50 +0530

    Feed back change part 3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import ApiEndPoint from "./ApiEndPoint";
 import axios from "axios";
 import { Card } from "antd";
 import "antd/dist/antd.css";
-import { FacebookSelector, FacebookSelectorEmoji } from "react-reactions";
-import {} from "react-facebook-emoji";
+import { FacebookSelector } from "react-reactions";
 import add from "./app/add.png";
 
-function App() {
-  const [orders, setOrders] = useState([]);
+function App() { 
   const [users, setUsers] = useState([]);
   const [reactions, setReactions] = useState([]);
   const [userContentReactions, setUserContentReactions] = useState([]);
@@ -18,17 +16,10 @@ function App() {
     for (let i = 1; i <= 30; i++)
       temp.push({ i, emoji: "", icon: "", emotions: "" });
     return temp;
-  };
-  const [localSelectedEmoji, setLocalSelectedEmoji] = useState(createData());
+  }; 
   const [count, setCount] = useState();
   const [current, setCurrent] = useState();
   const { Meta } = Card;
-
-  const fetchOrder = (ApiEndPoint) => {
-    axios.get(ApiEndPoint).then((data) => {
-      setOrders(data.data);
-    });
-  };
 
   const fetchUser = (ApiEndPoint) => {
     axios.get(ApiEndPoint).then((data) => {
@@ -42,15 +33,16 @@ function App() {
     });
   };
 
-  const settingEmojiCount = data => {
-    for (let i = 1; i <= 30; i++){
+  const settingEmojiCount = (data) => {
+    let arr = [];
+    for (let i = 1; i <= 30; i++) {
       const temp = data.filter((e) => e["user_id"] === i);
-      console.log(temp);
-      for(let j=0;j<temp.length;j++){
-        
-      }
+      let rec = { id: i, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
+      for (let j = 0; j < temp.length; j++) rec["" + temp[j]["reaction_id"]]++;
+      arr.push(rec);
     }
-  }
+    setCount(arr);
+  };
 
   const fetchUserContentReaction = (ApiEndPoint) => {
     axios.get(ApiEndPoint).then((data) => {
@@ -59,8 +51,7 @@ function App() {
     });
   };
 
-  useEffect(() => {
-    fetchOrder(ApiEndPoint + "orders");
+  useEffect(() => { 
     fetchUser(ApiEndPoint + "users");
     fetchReaction(ApiEndPoint + "reactions");
     fetchUserContentReaction(ApiEndPoint + "user_content_reactions");
@@ -68,43 +59,19 @@ function App() {
 
   const getEmojiId = (x) => {
     if (x === "like") return 1;
-    else if (x === "love") return 2;
-    else if (x === "haha") return 3;
-    else if (x === "wow") return 4;
-    else if (x === "sad") return 5;
-    else if (x === "angry") return 6;
-  };
+    else if (x === "love") return 0;
+    else if (x === "haha") return 2;
+    else if (x === "wow") return 3;
+    else if (x === "sad") return 4;
+    else if (x === "angry") return 5;
+  }; 
 
-  const images = [
-    { id: "like", img: "http://i.imgur.com/LwCYmcM.gif" },
-    { id: "love", img: "http://i.imgur.com/k5jMsaH.gif" },
-    { id: "haha", img: "http://i.imgur.com/f93vCxM.gif" },
-    { id: "yay", img: "http://i.imgur.com/a44ke8c.gif" },
-    { id: "wow", img: "http://i.imgur.com/9xTkN93.gif" },
-    { id: "sad", img: "http://i.imgur.com/tFOrN5d.gif" },
-    { id: "angry", img: "http://i.imgur.com/1MgcQg0.gif" },
-  ];
-
-  const setLocalEmoji = (id, reaction_id, emotions) => {
-    const icons = images.filter((d) => d["id"] === emotions);
-    const temp = reactions.filter((d) => d["id"] === reaction_id);
-    const newArr = [...localSelectedEmoji];
-    if (icons.length > 0) {
-      if (localSelectedEmoji.length == 0) {
-        newArr[id].emoji = temp.length > 0 ? temp[0]["emoji"] : "";
-        newArr[id].icon = icons[0]["img"];
-        newArr[id].emotions = emotions;
-      } else if (newArr[id].emotions === emotions) {
-        newArr[id].emoji = temp.length > 0 ? temp[0]["emoji"] : "";
-        newArr[id].icon = "";
-        newArr[id].emotions = "";
-      } else {
-        newArr[id].emoji = temp.length > 0 ? temp[0]["emoji"] : "";
-        newArr[id].icon = icons[0]["img"];
-        newArr[id].emotions = emotions;
-      }
-    }
-    setLocalSelectedEmoji(newArr);
+  const setLocalEmoji = (id, reaction_id, emotions) => { 
+    const newReaction = [...count];
+    newReaction.filter((d) => d["id"] === id)[0][reaction_id] == 1
+      ? (newReaction.filter((d) => d["id"] === id)[0][reaction_id] = 0)
+      : (newReaction.filter((d) => d["id"] === id)[0][reaction_id] = 1);
+    setCount(newReaction); 
   };
 
   const onSelect = (id, e) => {
@@ -118,6 +85,24 @@ function App() {
       .then(
         (res) => {
           console.log(res.data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
+  const onSelectUpdate = (id, e) => {
+    axios
+      .post(ApiEndPoint + "user_content_reactions", {
+        user_id: id,
+        reaction_id: getEmojiId(e),
+        content_id: 1,
+      })
+      .then(
+        (res) => {
+          console.log(res.data);
+          setLocalEmoji(id, getEmojiId(e), e);
         },
         (error) => {
           console.log(error);
@@ -146,47 +131,29 @@ function App() {
                   email={d["email"]}
                   id={d["id"]}
                 />
-                <div
-                  style={{
-                    display: "flex",
-                    padding: "2rem",
-                    justifyContent: "space-evenly",
-                    alignItems: "center",
-                    position: "relative",
-                  }}
+                <div className="emoji_section"
                 >
-                  {userContentReactions ? (
-                    userContentReactions
-                      .filter((emoji) => emoji["user_id"] === d["id"])
-                      .map((dbSelectedEmoji) =>
-                        reactions ? (
-                          reactions
-                            .filter(
-                              (rec) =>
-                                rec["id"] === dbSelectedEmoji["reaction_id"]
-                            )
-                            .map((emj) => (
-                              <span id={emj["id"]}>{emj["emoji"]}</span>
-                            ))
-                        ) : (
-                          <div id={d["id"]}>Loading emoji</div>
-                        )
-                      )
+                  {reactions ? (
+                    reactions.map((emj) => (
+                      <div id={emj["id"]}>
+                        {count.length > 0
+                          ? count
+                              .filter((x) => x["id"] === d["id"])
+                              .map((em) =>
+                                em[emj["id"]] != 0 ? (
+                                  <div className="emoji" key={emj["id"]}>
+                                    {emj["emoji"]} . {em[emj["id"]]}{" "}
+                                  </div>
+                                ) : (
+                                  <div></div>
+                                )
+                              )
+                          : ""}
+                      </div>
+                    ))
                   ) : (
-                    <div>Loading emoji</div>
-                  )}
-                  {localSelectedEmoji.length > 0 &&
-                  localSelectedEmoji[d["id"]]["icon"] != "" ? (
-                    <>
-                      <img
-                        style={{ width: "35px" }}
-                        src={localSelectedEmoji[d["id"]]["icon"]}
-                        id={d["id"]}
-                      />
-                    </>
-                  ) : (
-                    <div id={d["id"]}></div>
-                  )}
+                    <div id={d["id"]}>Loading emoji</div>
+                  )} 
                   {current === d["id"] ? (
                     <div
                       style={{
@@ -196,12 +163,8 @@ function App() {
                       }}
                     >
                       <FacebookSelector
-                        style={{
-                          zindex: "1",
-                          position: "absolute",
-                          top: "15rem",
-                        }}
-                        onSelect={(e) => onSelect(d["id"], e, e.img)}
+                        className="fb"
+                        onSelect={(e) => onSelectUpdate(d["id"], e, e.img)}
                         id={d["id"]}
                       />
                     </div>
@@ -210,8 +173,7 @@ function App() {
                   )}
                   <span>
                     <img
-                      src={add}
-                      style={{ width: "25px", paddingRight: "5px" }}
+                      src={add} className="emoji_selector"
                       onClick={(e) => enableEmojiSelector(d["id"], e)}
                     />
                   </span>
